3b70f424af2d310a227b7b125e6d70c9bfe3e2e9
ParkSeJun <chunsa1166@naver.com>
2020-12-06 08:52:30 +0900

    BGM 추가

---
+++ Assets/Scripts/BgmPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BgmPlayer : MonoBehaviour
+{
+    [SerializeField, Range(0.0f, 1.0f)] float volume;
+
+    AudioSource music;
+    float curVolume;
+
+    private void Awake()
+    {
+        music = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music.loop = true;
+        curVolume = volume;
+        music.volume = volume;
+        music.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (volume != curVolume)
+        {
+            music.volume = volume;
+            curVolume = volume;
+        }
+    }
+}
